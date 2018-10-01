ca435a141fd10844ee43c27f6f4e11f451ff373f
Chan Hong Wing <>
2018-10-01 15:11:33 +0800

    added some comment

--- 35A巴士到站.js
+++ 35A巴士到站.js
@@ -2,9 +2,9 @@
 var moment = require("moment");
 
 //http://etav3.kmb.hk/?action=geteta&lang=tc&route=35A&bound=1 d&stop_seq=4&servicetype=01
-var route = "35A";
-var bound = "1";
-var stop_seq = "4";
+var route = "35A";  //巴士號碼
+var bound = "1";    //巴士方向
+var stop_seq = "4"; //第幾個站 由0開始計
 
 $ui.loading("Loading...")
 $http.request({
@@ -19,7 +19,7 @@ $http.request({
     var arrivalTimeList = [];
     for (var key in data.response) {
       var now = moment(new Date());
-      var arrivalTime = moment(data.response[key].ex);
+      var arrivalTime = moment(data.response[key].ex); //KMB App貌似係用"t"，但我依度用了"ex"
 
       //arrivalTime.diff(now, "m") -> 相差時間(分鐘)
       arrivalTimeList.push(arrivalTime.diff(now, "m").toString());
